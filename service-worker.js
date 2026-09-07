23cac359cfe086ca35983e569ba49da0118b6d8b
Goldo <ton@email.com>
2026-09-07 14:36:37 +0200

    Migrate radar cache to IndexedDB to fix 5MB localStorage quota limit (v2.7.6)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service worker - Radars France PWA (Auto-Update Engine)
-const VERSION = 'v2.7.5';
+const VERSION = 'v2.7.6';
 const SHELL_CACHE = `shell-${VERSION}`;
 const RUNTIME_CACHE = `runtime-${VERSION}`;
 
