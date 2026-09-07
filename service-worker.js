62995de37c4e69d12f7da02ce999b54bc9921fa7
Goldo <ton@email.com>
2026-09-07 11:19:30 +0200

    Redesign Car Mode with authentic CamSam / Blitzer.de HUD interface (LED distance gauge, chevrons, speed signs, upcoming radars list & community bar)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service worker - Radars France PWA (Auto-Update Engine)
-const VERSION = 'v2.4.1';
+const VERSION = 'v2.5.0';
 const SHELL_CACHE = `shell-${VERSION}`;
 const RUNTIME_CACHE = `runtime-${VERSION}`;
 
